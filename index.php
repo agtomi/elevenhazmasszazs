343a9aa12b7cb4556afd50b08937ddbd15188110
agtomi <agtomi@gmail.com>
2018-07-10 21:31:41 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,98 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <head>
+        <title>Eleven Ház Fitness és Fallabda Órarend</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="description" content="Eleven Ház Fitness és Fallabda Órarend" />
+        <link rel="stylesheet" href="css/orarend.css" type="text/css" media="screen"/>
+    </head>
+
+    <body>
+       <div id="content">
+            <a class="back" href=""></a>
+                <span class="scroll"></span>
+                <div class="head"> 
+                    <div id="logo"></div>
+                    <div id="headszoveg">Masszázs Időbeosztása<br>2018. Április</div>
+                </div>
+           </div>
+            </tr>
+                      <tr>
+            <table class="table1">
+                <thead>
+                    <tr>
+<!--                        <th></th>-->
+                        <th scope="col" abbr="Hétfő">Hétfő</th>
+                        <th scope="col" abbr="Kedd">Kedd</th>
+                        <th scope="col" abbr="Szerda">Szerda</th>
+                        <th scope="col" abbr="Csütörtök">Csütörtök</th>
+                        <th scope="col" abbr="Péntek">Péntek</th>
+                        <th scope="col" abbr="Szombat">Szombat</th>
+                        <th scope="col" abbr="Vasárnap">Vasárnap</th>
+                        
+                    </tr>
+                </thead>
+               
+                <tbody>
+                      </tr>
+                  
+                         <tr>
+                        <td>Április 02.<br>HÚSVÉT</td></td>
+                        <td>Április 03.<br>Erzsi Reflexológia</><div class="ido">12:30 - 18:30</div></td>
+                        <td>Április 04.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 18:00</td>
+                        <td>Április 05.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 20:00</td></td>
+                        <td>Április 06.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 18:00</td>
+                        <td>Április 07.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">08:00 - 16:30</td>
+                        <td>Április 08.<br></td>
+                     <td></td>   
+                             <tr>   <tr>
+                                   <tr>
+                        <td>Április 09.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">11:30 - 20:00</td></td>
+                        <td>Április 10.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">08:00 - 12:00<br>Erzsi Reflexológia</><div class="ido">12:30 - 18:30</div></td>
+                        <td>Április 11.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 18:00</td>
+                        <td>Április 12.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 20:00</td></td>
+                        <td>Április 13.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">11:30 - 20:00</td>
+                        <td>Április 14.<br>Bálint<br>Sport és Gyógymasszázs</><div class="ido">08:00 - 16:30</td>
+                        <td>Április 15.<br></td>
+
+          <tr>   <tr>
+                                   <tr>
+                        <td>Április 16.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">11:30 - 20:00</td>
+                        <td>Április 17.<br>Erzsi Reflexológia</><div class="ido">12:30 - 18:30</div></td>
+                        <td>Április 18.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 18:00</td></td>
+                        <td>Április 19.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 20:00</td></td>
+                        <td>Április 20.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 18:00</td>
+                        <td>Április 21.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">08:00 - 16:30</td>
+                        <td>Április 22.</td>
+          <tr>   <tr>
+                                   <tr>
+                        <td>Április 23.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">11:30 - 20:00</td>
+                        <td>Április 24.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">08:00 - 12:00<br>Erzsi Reflexológia</><div class="ido">12:30 - 18:30</div></td>
+                        <td>Április 25.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 18:00</td>
+                        <td>Április 26.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">09:30 - 20:00</td></td>
+                        <td>Április 27.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">11:30 - 20:00</td>
+                        <td>Április 28.<br>Bálint<br>Sport és Gyógymasszázs</><div class="ido">08:00 - 16:30</td>
+                        <td>Április 29.<br></td>
+          <tr>   <tr>
+                                   <tr>
+                        <td>Április 30.<br>Vivien<br>Sport és Gyógymasszázs</><div class="ido">11:30 - 20:00</td></td>
+                        <td>Május 01.</td>
+                        <td>Május 02.</td>
+                        <td>Május 03.</td>
+                        <td>Május 04.</td>
+                        <td>Május 05.</td>
+                        <td>Május 06.</td>
+                </tbody>
+                           
+            </table>  
+           <div class="szoveg">
+               <p><b>Bejelentkezés:</b></p>
+               <p>Gyógymasszázs, sport és svédmasszázs: Tuza Vivien 06 57 430-551, 06 30 53-57-257  </p>                                    
+               <p>Gyógymasszázs, sport és svédmasszázs: Szappanos Bálint 06 57 430-551, 06 30 53-57-257 </p>
+               <p>Reflexológia : Zsótér Erzsébet : 06 20 8016899 </p>
+                  
+            </div>
+        </div>
+
+    </body>
+</html>
